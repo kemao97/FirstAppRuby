0db7aa605e5cbeee7b8869852bacfc49209bdc07
hoalt <kemao97@gmail.com>
2019-07-21 12:54:01 +0700

    fix convention and use i18n in page title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,5 @@
 module ApplicationHelper
-
-  # Returns the full title on a per-page basis.
-  def full_title(page_title = '')
+  def full_title page_title = ""
     base_title = "Ruby on Rails Tutorial Sample App"
     if page_title.empty?
       base_title
@@ -9,5 +7,4 @@ module ApplicationHelper
       page_title + " | " + base_title
     end
   end
-
 end
