9bcaedb4c1a472a14d549b5365637d47b358a470
hoalt <kemao97@gmail.com>
2019-07-22 09:42:33 +0700

    fix convention i18n for user, micropost, static_page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,14 +1,10 @@
 class StaticPagesController < ApplicationController
-
   def home
-    render layout: false
   end
 
   def help
-    render layout: false
   end
 
   def about
-    render layout: false
   end
 end
