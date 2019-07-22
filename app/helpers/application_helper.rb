ee78216efa6aacd07b22210b93888a9fcafab0b6
hoalt <kemao97@gmail.com>
2019-07-22 13:33:46 +0700

    fix convention use i18n lazy lookup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def full_title page_title = ""
-    base_title = t("application.base_title")
+    base_title = t "layouts.application.base_title"
     if page_title.empty?
       base_title
     else
